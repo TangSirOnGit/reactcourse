7bd24dba35b49747fa03b0d8b68c099d9d93eb37
laimingjiang <lai.mingjiang@bestv.com.cn>
2023-02-21 15:39:58 +0800

    Validation

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  }else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error);
@@ -29,14 +31,8 @@ const app = express();
 app.use(express.json());
 app.use(requestLogger);
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response,next) => {
   const body = request.body;
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "name or number is missing",
-    });
-  }
 
   const person = new Person({
     name: body.name,
@@ -45,7 +41,8 @@ app.post("/api/persons", (request, response) => {
 
   person.save().then((savedPerson) => {
     response.json(savedPerson);
-  });
+  })
+  .catch(error=>next(error));
 });
 
 app.get("/", (request, response) => {
